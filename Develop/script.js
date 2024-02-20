5ba543d6928208eee889463155ef7152fb971963
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-02-20 10:12:39 -0500

    Added loops and forced character

--- Develop/script.js
+++ Develop/script.js
@@ -5,70 +5,68 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-  
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var specialCharacters="~!@#$%^&*()_+{}|:<>?"
-var lowerCaseAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var upperCaseAlphabet="abcdefghijklmnopqrstuvwxyz"
-var numbers="1234567890"
+var specialCharacters = "~!@#$%^&*()_+{}|:<>?"
+var lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
+var upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "1234567890"
 
-function writePassword(){
-var specialCharactersPop=confirm("Do you want special character?")
-var lowerCaseAlphabetPop=confirm("Do you want lowercase alphabet?")
-var upperCaseAlphabetPop=confirm("Do you want uppercase alphabet?")
-var num=confirm("Do you want number?")
-var passLength=prompt("Enter a password length from 8 to 128 character")
+function generatePassword() {
+  var specialPop = confirm("Do you want special character?")
+  var lowerCasePop = confirm("Do you want lowercase alphabet?")
+  var upperCasePop = confirm("Do you want uppercase alphabet?")
+  var num = confirm("Do you want number?")
 
-if(passLength < 8 || passLength > 128){
-alert("Invalid Selection.Try again.!!");
-var passLength=prompt("Enter a password length from 8 to 128 character")
+  if (!specialPop && !lowerCasePop && !upperCasePop && !num) {
+    alert("You should choose altleast one");
+    return null
+  }
+
+  var passLength = Number(prompt("Enter a password length from 8 to 128 character"))
+
+  if (passLength < 8 || passLength > 128) {
+    alert("Invalid Selection.Try again.!!");
+    return null
+  }
+
+  var possibleCharacter = ""
+  var forcedCharacter = ""
+
+  if (specialPop) {
+    possibleCharacter += specialCharacters
+    var randomSpecialIndex = Math.floor(Math.random() * specialCharacters.length);
+    forcedCharacter += specialCharacters[randomSpecialIndex]
+  }
+
+  if (lowerCasePop) {
+    possibleCharacter += lowerCaseAlphabet
+    var randomLowerIndex = Math.floor(Math.random() * lowerCaseAlphabet.length);
+    forcedCharacter += lowerCaseAlphabet[randomLowerIndex]
+  }
+
+  if (upperCasePop) {
+    possibleCharacter += upperCaseAlphabet
+    var randomUpperIndex = Math.floor(Math.random() * upperCaseAlphabet.length);
+    forcedCharacter += upperCaseAlphabet[randomUpperIndex]
+  }
+
+  if (num) {
+    possibleCharacter += numbers
+    var randomUpperIndex = Math.floor(Math.random() * numbers.length);
+    forcedCharacter += numbers[randomUpperIndex]
+  }
+  console.log(forcedCharacter)
+  var password = ""
+  for (var i = 0; i < passLength - forcedCharacter.length; i++) {
+    var randomPassIndex = Math.floor(Math.random() * possibleCharacter.length);
+    password += possibleCharacter[randomPassIndex]
+  }
+  return password + forcedCharacter
 }
 
-else if(specialCharactersPop ===false && lowerCaseAlphabetPop===false && upperCaseAlphabetPop==false && num===false){
-  alert("You should choose altleast one");
-var specialCharactersPop=confirm("Do you want special character?")
-var lowerCaseAlphabetPop=confirm("Do you want lowercase alphabet?")
-var upperCaseAlphabetPop=confirm("Do you want uppercase alphabet?")
-var num=confirm("Do you want number?")
-}
-
-
- if(specialCharactersPop==false){
-  upperCaseAlphabet + lowerCaseAlphabet + numbers
- }
-
- if(lowerCaseAlphabetPop==false){
-  specialCharacters+ upperCaseAlphabet + numbers
- }
-
- if(upperCaseAlphabet==false){
-  specialCharacters+ lowerCaseAlphabet + numbers
- }
-
- if(numbers== false){
-  specialCharacters+ upperCaseAlphabet + lowerCaseAlphabet
- }
-
-
- for (var i=0; i<passLength; i++){
- var randomPass= Math.floor(Math.random()* i.length); 
- }return (randomPass);
- 
-}
-
-//  function generatepass(){
-//   var charset= specialCharacters+ upperCaseAlphabet+ lowerCaseAlphabet+ numbers;
-//   var password="";
-//   for ( var i=0; i<passLength; i++){
-//     var randomIndex= Math.floor(Math.random()* charset.length)
-//     password += charset[randomIndex];
-//   }return password;
-// }
-// generatepass()
-// }
